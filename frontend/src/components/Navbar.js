44206fe1122b7335c77505967247515ec108d009
GNLimerence <phamhoanghainam203@gmail.com>
2024-12-04 15:47:25 +0700

    feat: add profile

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Container, Row, Col, NavDropdown } from "react-bootstrap";
+import { Container, Row, Col, NavDropdown, Image } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getProfileByUserId } from "../services/API";
 import {
   faEnvelope,
   faBell,
@@ -25,6 +26,15 @@ export default function NavBar({
   const navigate = useNavigate();
 
   const [selectedIcon, setSelectedIcon] = useState("home");
+
+  const [profile, setProfile] = useState();
+
+  React.useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    getProfileByUserId(userId)
+      .then((res) => setProfile(res.data.user))
+      .catch((err) => console.error("API error: ", err));
+  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem("userId");
@@ -60,26 +70,43 @@ export default function NavBar({
   };
 
   return (
-    <Container className='p-2 border-bottom sticky-top' fluid style={{ backgroundColor: '#e0e0e0',boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)' }}>
-      <Row className='align-items-center'>
-        <Col className='col-3 d-flex align-items-center'>
-          <Link className='me-0' to={'/'} style={{ backgroundColor: '#e0e0e0'}}>
-          <div style={{
-    backgroundColor: '#e0e0e0',  // Màu nền bạn muốn
-    width: '50px',
-    height: '50px',
-    display: 'flex',
-    alignItems: 'center',  // Canh giữa theo chiều dọc
-    justifyContent: 'center',  // Canh giữa theo chiều ngang
-    borderRadius: '50%'  // Nếu muốn làm tròn nền ảnh
-  }}>
-    <img src={logo} alt="Logo" style={{
-      width: '50px',
-      height: '50px',
-      objectFit: 'cover',  // Đảm bảo ảnh không bị méo
-      borderRadius: '50%'  // Làm tròn ảnh nếu muốn
-    }} />
-  </div>
+    <Container
+      className="p-2 border-bottom sticky-top"
+      fluid
+      style={{
+        backgroundColor: "#e0e0e0",
+        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <Row className="align-items-center">
+        <Col className="col-3 d-flex align-items-center">
+          <Link
+            className="me-0"
+            to={"/"}
+            style={{ backgroundColor: "#e0e0e0" }}
+          >
+            <div
+              style={{
+                backgroundColor: "#e0e0e0", // Màu nền bạn muốn
+                width: "50px",
+                height: "50px",
+                display: "flex",
+                alignItems: "center", // Canh giữa theo chiều dọc
+                justifyContent: "center", // Canh giữa theo chiều ngang
+                borderRadius: "50%", // Nếu muốn làm tròn nền ảnh
+              }}
+            >
+              <img
+                src={logo}
+                alt="Logo"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  objectFit: "cover", // Đảm bảo ảnh không bị méo
+                  borderRadius: "50%", // Làm tròn ảnh nếu muốn
+                }}
+              />
+            </div>
           </Link>
           <SearchBar />
         </Col>
@@ -166,7 +193,8 @@ export default function NavBar({
                   transition: "transform 0.2s ease, background-color 0.3s ease",
                   padding: "10px",
                   borderRadius: "50%",
-                  backgroundColor: selectedIcon === "user" ? "#e6f7ff" : "transparent",
+                  backgroundColor:
+                    selectedIcon === "user" ? "#e6f7ff" : "transparent",
                 }}
                 data-icon="user"
                 onMouseEnter={(e) => iconHoverStyle(e, true)}
@@ -174,14 +202,27 @@ export default function NavBar({
               />
             }
             id="basic-nav-dropdown"
-            align="end" 
+            align="end"
           >
             <NavDropdown.Item onClick={handleToProfile}>
-              Profile
+              <div className="d-flex align-items-center">
+                <Image
+                  src={profile.avatar}
+                  roundedCircle
+                  style={{
+                    width: "40px",
+                    height: "40px",
+                    objectFit: "cover",
+                    marginRight: "10px",
+                  }}
+                />
+                <span style={{ fontWeight: "bold", fontSize: "14px" }}>
+                  {profile.firstName + " " + profile.lastName}
+                </span>
+              </div>
             </NavDropdown.Item>
-            <NavDropdown.Item onClick={handleLogout}>
-              Đăng xuất
-            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
           </NavDropdown>
         </Col>
       </Row>
