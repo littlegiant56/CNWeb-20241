998cd2c58ef4d4c4f089eecb9d5be72d86928e43
GNLimerence <phamhoanghainam203@gmail.com>
2024-12-05 00:14:35 +0700

    feat: add proxy

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://web-20232.onrender.com"
+    ? "https://fonapi.namphamdev.id.vn"
     : "http://localhost:5000";
 
 export function register(email, password) {
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,11 +1,14 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-const URL = process.env.NODE_ENV === 'production' ? "https://web-20232.onrender.com" : 'http://localhost:5000';
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://fonapi.namphamdev.id.vn"
+    : "http://localhost:5000";
 
 // Create a socket connection
 export const socket = io(URL, {
   query: {
-    userId: localStorage.getItem('userId')
+    userId: localStorage.getItem("userId"),
   },
-  autoConnect: false
+  autoConnect: false,
 });
